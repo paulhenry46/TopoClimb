49fe0d7b1269143fdd8ddb8598b9e0264581cbcf
Paulhenry <paulhenrysaux@gmail.com>
2025-02-16 09:53:37 +0100

    wip

--- resources/views/livewire/areas/wizard-2.blade.php
+++ resources/views/livewire/areas/wizard-2.blade.php
@@ -92,8 +92,8 @@ new class extends Component {
         <div class="rounded-md bg-indigo-50 p-4">
           <div class="flex">
             <div class="flex-shrink-0">
-              <svg class="h-5 w-5 text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z" clip-rule="evenodd" />
+              <svg class="h-5 w-5 text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+                <path d="m424-408-86-86q-11-11-28-11t-28 11q-11 11-11 28t11 28l114 114q12 12 28 12t28-12l226-226q11-11 11-28t-11-28q-11-11-28-11t-28 11L424-408Zm56 328q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
               </svg>
             </div>
             <div class="ml-3">
@@ -131,7 +131,6 @@ new class extends Component {
     };
     project.importSVG('{{$this->url}}', function() {
         project.activeLayer.fitBounds(view.bounds);
-        console.log(project.activeLayer.exportJSON());
 
         for (var item of project.activeLayer.getItems({
                 class: Path
@@ -142,7 +141,6 @@ new class extends Component {
             createNewSeparator(item);
 
         }
-        console.log(number_sectors)
     });
     document.addEventListener('restart', () => {
         project.clear()
@@ -152,7 +150,6 @@ new class extends Component {
 
         project.importSVG('{{$this->url}}', function() {
             project.activeLayer.fitBounds(view.bounds);
-            console.log(project.activeLayer.exportJSON());
 
             for (var item of project.activeLayer.getItems({
                     class: Path
@@ -163,25 +160,23 @@ new class extends Component {
                 createNewSeparator(item);
 
             }
-            console.log(number_sectors)
         });
 
     })
 
     document.addEventListener('terminated', () => {
-
+        if(number_processing - 1 == number_sectors){
         exportTheProject();
-
         var evt = new CustomEvent('sent_to_wire', {
             detail: {
                 message: 'ok',
             }
         });
         window.dispatchEvent(evt);
+      }
     })
 
     function createTruc(point) {
-        // Add a segment to the path at the position of the mouse:
         point_cool = point;
 
         var circle = new Path.Circle({
@@ -191,14 +186,13 @@ new class extends Component {
         });
         circle.name = 'circle_' + number_processing;
         point_cool.y = point_cool.y + 4;
+
         var text = new PointText(point_cool);
         text.fillColor = 'black';
-
-
-        // Set the content of the text item:
         text.content = number_processing;
         text.name = 'text_' + number_processing;
         text.justification = "center";
+
         var box = new Path.Rectangle({
             center: circle.position,
             size: [diameter - 8, diameter - 8],
@@ -206,13 +200,12 @@ new class extends Component {
         });
         text.fitBounds(box.bounds);
         box.remove();
+
         group = new Group([circle, text]);
         group.name = 'group_' + number_processing;
     }
 
     function createNewSeparator(item) {
-        console.log('coucou')
-        // Add a segment to the path at the position of the mouse:
         start = item.getPointAt(item.length - 10)
         end = item.getPointAt(item.length) + item.getTangentAt(item.length - 1) * 2
 
@@ -230,18 +223,6 @@ new class extends Component {
         path.name = 'separator';
     }
 
-    function createSeparator(point) {
-        // Add a segment to the path at the position of the mouse:
-        point_cool = point;
-
-        var circle = new Path.Circle({
-            center: point_cool,
-            radius: 10,
-            fillColor: color
-        });
-        circle.name = 'separator_' + number_processing;
-    }
-
     function onMouseDown(event) {
         segment = path = null;
         var hitResult = project.hitTest(event.point, hitOptions);
@@ -251,9 +232,7 @@ new class extends Component {
 
         if (hitResult) {
             hit = true;
-            console.log('ok')
             path = hitResult.item;
-            console.log(path)
             if (number_processing > number_sectors) {
                 var evt = new CustomEvent('svg_with_numbers', {
                     detail: {
@@ -264,13 +243,9 @@ new class extends Component {
                 });
                 window.dispatchEvent(evt);
 
-                console.log('doing that');
-                console.log(project.activeLayer.exportJSON());
-
                 for (var item of project.activeLayer.getItems({
                         class: Path
                     })) {
-                    console.log(item.name);
                     if (item.name.replace(/[^a-z]/g, '') == 'text' || item.name.replace(/[^a-z]/g, '') == 'circle') {
                         item.remove();
 
@@ -280,10 +255,8 @@ new class extends Component {
                 for (var item of project.activeLayer.getItems({
                         class: PointText
                     })) {
-                    console.log(item.name);
                     item.remove();
                 }
-                console.log(project.activeLayer.exportJSON());
                 var evt = new CustomEvent('svg_edited', {
                     detail: {
                         message: project.activeLayer.exportSVG({
@@ -294,7 +267,6 @@ new class extends Component {
                 window.dispatchEvent(evt);
                 return;
             }
-
 
             if (sectors_numbered.includes(path.name)) {
                 return;
@@ -307,9 +279,7 @@ new class extends Component {
             path.name = 'sector_' + number_processing;
             sectors_numbered.push(path.name);
             number_processing++;
-            console.log(sectors_numbered);
             project.activeLayer.fitBounds(view.bounds);
-
         }
     }
 
@@ -324,14 +294,9 @@ new class extends Component {
         });
         window.dispatchEvent(evt);
 
-        console.log(project.activeLayer.exportJSON());
-
-
-
         for (var item of project.activeLayer.getItems({
                 class: Path
             })) {
-            console.log(item.name);
             if (item.name.replace(/[^a-z]/g, '') == 'text' || item.name.replace(/[^a-z]/g, '') == 'circle') {
                 item.remove();
 
@@ -341,10 +306,8 @@ new class extends Component {
         for (var item of project.activeLayer.getItems({
                 class: PointText
             })) {
-            console.log(item.name);
             item.remove();
         }
-        console.log(project.activeLayer.exportJSON());
         var evt = new CustomEvent('svg_edited', {
             detail: {
                 message: project.activeLayer.exportSVG({
@@ -359,7 +322,6 @@ new class extends Component {
         for (var item of project.activeLayer.getItems({
                 class: Path
             })) {
-            console.log(item.name);
             if (item.name.replace(/[^a-z]/g, '') == 'circle') {
 
                 var box = new Path.Rectangle({
@@ -376,7 +338,6 @@ new class extends Component {
         for (var item of project.activeLayer.getItems({
                 class: PointText
             })) {
-            console.log(item.name);
             if (item.name.replace(/[^a-z]/g, '') == 'text') {
 
                 var box = new Path.Rectangle({
