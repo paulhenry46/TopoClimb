fef9cd0e364d4c79d688cd4f1423b3e2979ad29f
Paulhenry <paulhenrysaux@gmail.com>
2025-02-27 10:47:02 +0100

    wip

--- resources/views/livewire/routes/manager.blade.php
+++ resources/views/livewire/routes/manager.blade.php
@@ -30,6 +30,8 @@ new class extends Component {
     #[Validate('required')]
     public $date;
 
+    public $creators;
+
     public $slug;
     public $id_editing;
     public $lines;
@@ -266,6 +268,58 @@ new class extends Component {
               <x-input-error for="date" class="mt-2" />
             </div>
           </div>
+          <div class="space-y-2 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+            <x-label for="creators" value="{{ __('Others opener') }}" />
+          <div class="sm:col-span-2">
+            <x-input wire:model="creators" type="text" name="creators" id="project-name" class="block w-full" />
+            <x-input-error for="date" class="mt-2" />
+          </div>
+        </div>
+        <div class="space-y-2 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+          <x-label for="creators" value="{{ __('Tags') }}" />
+        <div @click.outside="showListe = false" class="sm:col-span-2" 
+        x-data="{tags: [{ id: 1, tag: 'archées' },
+      { id: 2, tag: 'force' }, { id: 3, tag: 'devers' }], 
+       SelectedID: [], 
+       term : '',
+       showListe: false, 
+       toogle(id){
+                if (this.SelectedID.includes(id)) {
+                    this.SelectedID = this.SelectedID.filter(item => item !== id);
+                } else {
+                    this.SelectedID.push(id);
+                }
+            }
+        }"  >
+          <div>
+            <div class="relative mt-2">
+              <input x-model="term" @click="showListe = true"  id="combobox" type="text" class="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-12 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" role="combobox" aria-controls="options" aria-expanded="false">
+              <button type="button" class="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
+                <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fill-rule="evenodd" d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" clip-rule="evenodd" />
+                </svg>
+              </button>
+          
+              <ul x-show="showListe" class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm" id="options" role="listbox">
+                
+                <template x-for="tag in tags">
+                <li x-show="!(term.length > 0 && !tag['tag'].includes(term))" :class="SelectedID.includes(tag['id']) ? 'font-semibold' : 'text-gray-900'"  @click="toogle(tag['id'])" class="hover:bg-gray-100 relative cursor-default select-none py-2 pl-8 pr-4 text-gray-900" id="option-0" role="option" tabindex="-1">
+                  <!-- Selected: "font-semibold" -->
+                  <span class="block truncate" x-text="tag['tag']"></span>
+                  <span :class="SelectedID.includes(tag['id']) ? 'text-gray-600' : ' hidden'" class="absolute inset-y-0 left-0 flex items-center pl-1.5 text-gray-600">
+                    <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                    </svg>
+                  </span>
+                </li>
+              </template>
+                <!-- More items... -->
+              </ul>
+            </div>
+          </div>
+          <x-input-error for="date" class="mt-2" />
+        </div>
+      </div>
           <!-- Tags pour le style de voie, combobox pour les ouvreurs, dessin sur schema selon le secteur-->
                 </div>
               </div>
