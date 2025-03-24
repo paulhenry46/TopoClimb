88cfd79de94cd29c5f3425a976e6e829dd6d3f9d
Paulhenry <paulhenrysaux@gmail.com>
2025-03-24 09:48:27 +0100

    wip

--- resources/views/livewire/areas/view.blade.php
+++ resources/views/livewire/areas/view.blade.php
@@ -280,25 +280,48 @@ new class extends Component {
                   </button>                  
                 </div>
               </div>
-              <div class="grid grid-cols-2 mt-4 gap-x-6">
-                <div class="text-gray-500 mt-4 flex w-full flex-none gap-x-4">
+              <div class="grid grid-cols-3 mt-4 gap-x-2">
+                <div class="text-gray-500 mt-4 flex w-full flex-none gap-x-2">
                   <dt class="flex-none">
                     <span class="sr-only">Mail</span>
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M480-440q58 0 99-41t41-99q0-58-41-99t-99-41q-58 0-99 41t-41 99q0 58 41 99t99 41ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-46q-54-53-125.5-83.5T480-360q-83 0-154.5 30.5T200-246v46Z"/></svg>
                   </dt>
                   <dd class="text-sm leading-6 ">
-                    By 
+                     
                     @foreach ($this->route->users as $user)
                       {{ $user->name }}
                     @endforeach
                   </dd>
                 </div>
-                <div class="text-gray-500 mt-4 flex w-full flex-none gap-x-4">
+                <div class="text-gray-500 mt-4 flex w-full flex-none gap-x-2">
                   <dt class="flex-none">
                     <span class="sr-only">Mail</span>
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M360-300q-42 0-71-29t-29-71q0-42 29-71t71-29q42 0 71 29t29 71q0 42-29 71t-71 29ZM200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-40q0-17 11.5-28.5T280-880q17 0 28.5 11.5T320-840v40h320v-40q0-17 11.5-28.5T680-880q17 0 28.5 11.5T720-840v40h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Z"/></svg>
                   </dt>
-                  <dd class="text-sm leading-6 ">The {{ $this->route->created_at->format('d/m/y') }}</dd>
+                  <dd class="text-sm leading-6 ">{{ $this->route->created_at->format('d/m/y') }}</dd>
+                </div>
+                <div class="text-gray-500 mt-4 flex w-full flex-none gap-x-2">
+                  <dt class="flex-none">
+                    <span class="sr-only">Mail</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M480-269 314-169q-11 7-23 6t-21-8q-9-7-14-17.5t-2-23.5l44-189-147-127q-10-9-12.5-20.5T140-571q4-11 12-18t22-9l194-17 75-178q5-12 15.5-18t21.5-6q11 0 21.5 6t15.5 18l75 178 194 17q14 2 22 9t12 18q4 11 1.5 22.5T809-528L662-401l44 189q3 13-2 23.5T690-171q-9 7-21 8t-23-6L480-269Z"/></svg>
+                  </dt>
+                  <dd class="text-sm leading-6 ">{{ $this->route->gradeFormated() }}</dd>
+                </div>
+                <div class="text-gray-500 mt-5 flex w-full flex-none gap-x-2">
+                  <dt class="flex-none">
+                    <span class="sr-only">Mail</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M856-390 570-104q-12 12-27 18t-30 6q-15 0-30-6t-27-18L103-457q-11-11-17-25.5T80-513v-287q0-33 23.5-56.5T160-880h287q16 0 31 6.5t26 17.5l352 353q12 12 17.5 27t5.5 30q0 15-5.5 29.5T856-390ZM260-640q25 0 42.5-17.5T320-700q0-25-17.5-42.5T260-760q-25 0-42.5 17.5T200-700q0 25 17.5 42.5T260-640Z"/></svg>
+                  </dt>
+                  <dd class="text-sm leading-6 flex gap-x-1">
+                    @foreach ($this->route->tags as $tag)
+                    <span class=" mr-2 inline-flex items-center gap-x-1.5 rounded-md bg-gray-100 px-2 py-1 text-xs font-medium text-gray-700">
+                      <svg class="h-1.5 w-1.5 fill-gray-500" viewBox="0 0 6 6" aria-hidden="true">
+                        <circle cx="3" cy="3" r="3"></circle>
+                      </svg>
+                      {{ $tag->name }}
+                    </span>
+                    @endforeach
+                  </dd>
                 </div>
               </div>
             </div>
