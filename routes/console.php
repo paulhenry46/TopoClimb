6f367d062d4f8c1905201b680f3ceca408f52f0c
Paulhenry <paulhenrysaux@gmail.com>
2025-04-27 20:41:49 +0200

    wip

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Models\User;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Artisan::command('grant_owner', function () {
+   $users = User::all();
+   foreach($users as $user){
+    $user->assignRole('owner');
+   }
+})->purpose('Temporary DEV command to grant all access to everybody');
