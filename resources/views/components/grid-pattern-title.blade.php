729121a475ac9d5223343f1434357d55905c7b2e
Paulhenry <paulhenrysaux@gmail.com>
2025-05-22 21:10:20 +0200

    ui fix

--- resources/views/components/grid-pattern-title.blade.php
+++ resources/views/components/grid-pattern-title.blade.php
@@ -1,3 +1,4 @@
-<div class="md:absolute hidden md:block -ml-16 mt-18">
-    <p class="font-semibold text-right origin-top-left  -rotate-90 text-gray-600 font-mono text-[0.8125rem]/6 font-medium tracking-widest text-pretty uppercase ">{{ $slot }}</p>           
+<div class="md:absolute hidden md:block -ml-10 mt-2">
+    <p class="font-semibold -left-10 origin-bottom-right -translate-x-full -translate-y-full  -rotate-90 text-gray-600 font-mono text-[0.8125rem]/6 font-medium tracking-widest text-pretty uppercase ">{{ $slot }}</p>           
 </div>
+
